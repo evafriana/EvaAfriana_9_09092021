3476e2e1e9de66104e2a0f3a8a37e60a6d731ff2
Eva Afriana <vava.afriana@gmail.com>
2021-09-25 17:19:19 +0200

    test icon highlited and navigate new bills page are not finished

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,21 +1,37 @@
-import { screen } from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
+import { screen } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import VerticalLayout from "../views/VerticalLayout";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
+      const html = BillsUI({ data: [] });
+      document.body.innerHTML = html;
       //to-do write expect expression
-    })
+      const icon = screen.getByTestId("icon-window");
+      expect(icon).toHaveClass("active-icon");
+    });
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
-      document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
-  })
-})
+      const html = BillsUI({ data: bills });
+      document.body.innerHTML = html;
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
+
+    describe("And I click on new bill button", () => {
+      test("then should navigate to new bills page"),
+        () => {
+          const html = BillsUI({ data: [] });
+          document.body.innerHTML = html;
+          //to-do write expect expression
+        };
+    });
+  });
+});
